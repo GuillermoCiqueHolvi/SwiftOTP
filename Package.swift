01eed298741450340f2f3d435e3598d7e1a173db
Guillermo Cique <guillermo@holvi.com>
2026-06-09 15:59:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "SwiftOTP",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+		.iOS(.v15), .macOS(.v11), .tvOS(.v15), .watchOS(.v9), .visionOS(.v1)
     ],
     products: [
         .library(name: "SwiftOTP", targets: ["SwiftOTP"]),
         .library(name: "SwiftOTP-Dynamic", type: .dynamic, targets: ["SwiftOTP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0" ..< "5.0.0")
+		.package(url: "https://github.com/apple/swift-crypto.git", branch: "5.x")
     ],
     targets: [
-        .target(name: "SwiftOTP", dependencies: ["Crypto"], path: "SwiftOTP/"),
+        .target(name: "SwiftOTP", dependencies: [.product(name: "Crypto", package: "swift-crypto")], path: "SwiftOTP/"),
         .testTarget(name: "SwiftOTPTests", dependencies: ["SwiftOTP"])
     ]
 )
